c122efd18fa068557a0c5e9e89f9e197db91fe5c
Marie Moser-Schwaiger <marie.moser-schwaiger@stud.sbg.ac.at>
2021-05-14 10:01:03 +0200

    random key generator + enrollment and authentication

--- src/FuzzyCommitment.java
+++ src/FuzzyCommitment.java
@@ -5,6 +5,25 @@ import java.util.Arrays;
 
 public class FuzzyCommitment {
 
+    //mit de indexe geht des eher ned so, ma kann ja mehrere enrollen, woher weiß i dann welches delta i brauch
+    public String[][] enrollment;
+    public int indexEnrollment;
+
+    public String[][] authentication;
+    public int indexAuthentication;
+
+    public String delta;
+
+    public FuzzyCommitment(){
+        enrollment = new String[10][2];
+        indexEnrollment = 0;
+
+        authentication = new String[10][2];
+        indexAuthentication = 0;
+
+        delta = "";
+    }
+
     public String xorStrings(String inputData, String key){
         StringBuilder xorString = new StringBuilder();
 
@@ -14,7 +33,7 @@ public class FuzzyCommitment {
             }
         }
         else
-            throw new RuntimeException("Given Strings haven't the same length -> but needed for XOR-operation \n" +
+            throw new RuntimeException("Given Strings do not have the same length -> same length needed for XOR-operation \n" +
                     "input data: " + inputData + "\n  length = " + inputData.length() +
                     "key data: " + key+ "\n  length = " + key.length());
 
@@ -28,5 +47,64 @@ public class FuzzyCommitment {
         System.out.println(Arrays.toString(encodedhash));
 
         return new String(encodedhash);
+    }
+
+    public void enrollment (String biometricData) throws NoSuchAlgorithmException {
+
+        // key and hash of key
+        KeyGenerator keyGen = new KeyGenerator();
+        String key = keyGen.randomKey(1172);
+        String hashKey = SHA256hashing(key);
+
+        // ecc of key
+        ECCHamming ecc = new ECCHamming();
+        String eccKey = ecc.addParity(key);
+
+        // expansion of biometric data
+        String data = "000" + biometricData;
+
+        // xor of data and key (delta)
+        String xor = xorStrings(data, eccKey);
+
+        enrollment[indexEnrollment][0] = hashKey;
+        enrollment[indexEnrollment][1] = xor;
+
+        indexEnrollment++;
+
+
+        //nur für speichern mal
+        delta = xor;
+    }
+
+    public void authentication (String biometricData) throws NoSuchAlgorithmException {
+        ECCHamming ecc = new ECCHamming();
+
+        // expansion of biometric data
+        String tryData = "000" + biometricData;
+
+        // xor of data and delta
+        String xor = xorStrings(tryData, delta);
+
+        // decode
+        String tryKey = ecc.parityRemove(xor, 0);
+
+        // hash
+        String hashKey = SHA256hashing(tryKey);
+
+        authentication[indexAuthentication][0] = hashKey;
+        authentication[indexAuthentication][1] = delta;
+
+        indexAuthentication++;
+
+        /*
+        //überlegung für authentication
+        for (int i = 0; i < 10; i++){
+            delta = enrollment[i][1]
+            newKey = xor(tryData, delta)
+            newKey.removeparity
+            newHash = hash(xor)
+            wenn newHash == enrollment[i][0] --> successsss
+        }
+        */
     }
 }
---
+++ src/KeyGenerator.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class KeyGenerator {
+
+    public String randomKey(int lengthOfKey){
+        StringBuilder randKey = new StringBuilder();
+
+        for(int i = 0; i < lengthOfKey; i++){
+            double rand = Math.random();
+            if (rand <= 0.5)
+                randKey.append('0');
+            else
+                randKey.append('1');
+        }
+
+        return randKey.toString();
+    }
+
+}
+
+
